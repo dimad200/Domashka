12062d0f181bda5d7c5abedf02f87b63aa0bd070
dimad200 <100908404+dimad200@users.noreply.github.com>
2024-10-14 19:44:34 +0400

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,2 +1,8 @@
-name =='Dima'
-print(name, type(name))
+name ='Dima'
+print("Name: ", name)
+age= 41
+print('Age: ',age)
+age=age+99
+print('New Age: ',age)
+is_student=True
+print('Is Student:',is_student)
