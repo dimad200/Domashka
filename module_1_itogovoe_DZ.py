81516664c6d95e25c96efa9fc6707cbaaca49c70
dimad200 <dimad@udm.ru>
2024-10-21 10:33:52 +0400

    домашка вся по модулю 1!

--- module_1_itogovoe_DZ.py
+++ module_1_itogovoe_DZ.py
@@ -4,7 +4,6 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 sorted_students=sorted(students)
 # создам список с средними оценками:
 Sredniy_bal=[sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/len(grades[2]),sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4]),]
-print(Sredniy_bal)
 #Создам итоговый словарь:
 Itog={sorted_students[0]:Sredniy_bal[0],sorted_students[1]:Sredniy_bal[1],sorted_students[2]:Sredniy_bal[2],sorted_students[3]:Sredniy_bal[3],sorted_students[4]:Sredniy_bal[4]}
 print(Itog)
