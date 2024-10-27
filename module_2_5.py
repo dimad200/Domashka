07f8baaec7a58d76dac282428dc343e24e3aaaf6
dimad200 <dimad@udm.ru>
2024-10-27 21:15:52 +0400

    добавлен module_2_5

--- module_2_5.py
+++ module_2_5.py
@@ -5,9 +5,7 @@ def get_matrix(n=1,m=1,value=1):
         for k in range (m):
             matrix[i].append(value)
     return matrix
-
-
-#
+# начало программы
 get_matrix()
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
