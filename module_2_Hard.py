94680f40cca186bb3d396a974fd3589ad10e47d8
dimad200 <dimad@udm.ru>
2024-10-29 17:23:13 +0400

    Исправлен ответ на сложный модуль

--- module_2_Hard.py
+++ module_2_Hard.py
@@ -1,8 +1,6 @@
-def Search_duble(list,a,b):
-    print('list=', list)
-    c = (a in list) # проверка что a входит в список
-    d = (b in list) # проверка что b  входит в список
-    print('a=', a, 'b=', b, 'c=', c, 'd=', d)
+def Search_duble(list,a,b): # Функция для поиска дублей
+    c = (a in list) # Флаг, что a входит в список
+    d = (b in list) # Флаг, что b входит в список
     e=((c==True) and (d==True))
     if (e==False):
         list.append(a)
@@ -19,29 +17,24 @@ def Search_duble(list,a,b):
     return list
 
 
-print ("Введите выпавшее число")
-number=int(input())
-List_for_search=[]
-result=[]
+print("Введите выпавшее число")
+number = int(input())
+List_for_search = []
+result = []
 # Заполню список для перебора
-for i in range(number-1):
-  List_for_search.append(i+1)
+for i in range(number - 1):
+    List_for_search.append(i + 1)
 # Заполню список результами преобразований 
 for i in range(len(List_for_search)):
-  internal_list = List_for_search.copy()
-  a=internal_list.pop(i)
-  for k in range(len(internal_list)):
-    b=internal_list[k]    
-    if number%(a+b)==0: #Проверка на соответствие результатов
-      Search_duble(result,a,b)
-      # #
-      # c=(a in result) and (b in result)
-      # if c==False:
-      #   result.append(a)
-      #   result.append(b)
+    internal_list = List_for_search.copy()
+    a = internal_list.pop(i)
+    for k in range(len(internal_list)):
+        b = internal_list[k]
+        if number % (a + b) == 0:  #Проверка на соответствие результатов
+            Search_duble(result, a, b)  # поиск дублирующих пар
 # Приведу результат к виду, который указан в задании
-result_str=""
+result_str = ""
 #print("result=",result)
 for i in range(len(result)):
-  result_str=result_str+str(result[i])
-print (result_str)
+    result_str = result_str + str(result[i])
+print(result_str)
