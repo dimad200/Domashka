b12e1b1b203abe7d8c4c4f51e673be0c97dbb4f8
dimad200 <dimad@udm.ru>
2024-10-29 18:18:06 +0400

    Исправлен ответ на сложный модуль

--- module_2_Hard.py
+++ module_2_Hard.py
@@ -1,37 +1,36 @@
-def Search_duble(list,a,b): # Функция для поиска дублей
-    c = (a in list) # Флаг, что a входит в список
-    d = (b in list) # Флаг, что b входит в список
-    e=((c==True) and (d==True))
-    if (e==False):
-        list.append(a)
-        list.append(b)
+def search_double(list_,a,b): # Функция для поиска дублей
+    c = (a in list_) # Флаг, что a входит в список
+    d = (b in list_) # Флаг, что b входит в список
+    e= ((c == True) and (d == True))
+    if not e:
+        list_.append(a)
+        list_.append(b)
     if c==True and d==True:
-        Flag_1=False
-        for i in range(0,len(list),2):
-            if b==list[i] and a==list[i+1]:
-                Flag_1=True
-        if Flag_1==False:
-            list.append(a)
-            list.append(b)
-
-    return list
+        flag_1 = False # флаг вхождения пары в список
+        for i in range(0,len(list_),2):
+            if b==list_[i] and a==list_[i+1]:
+                flag_1 = True
+        if not flag_1:
+            list_.append(a)
+            list_.append(b)
+    return list_
 
 
 print("Введите выпавшее число")
 number = int(input())
-List_for_search = []
+list__for_search = []
 result = []
 # Заполню список для перебора
 for i in range(number - 1):
-    List_for_search.append(i + 1)
-# Заполню список результами преобразований 
-for i in range(len(List_for_search)):
-    internal_list = List_for_search.copy()
-    a = internal_list.pop(i)
-    for k in range(len(internal_list)):
-        b = internal_list[k]
+    list__for_search.append(i + 1)
+# Заполню список результатами преобразований
+for i in range(len(list__for_search)):
+    internal_list_ = list__for_search.copy()
+    a = internal_list_.pop(i)
+    for k in range(len(internal_list_)):
+        b = internal_list_[k]
         if number % (a + b) == 0:  #Проверка на соответствие результатов
-            Search_duble(result, a, b)  # поиск дублирующих пар
+            search_double(result, a, b)  # поиск дублирующих пар
 # Приведу результат к виду, который указан в задании
 result_str = ""
 #print("result=",result)
