6eadaa4af63208463d5651d71c80a79d1843d030
dimad200 <dimad@udm.ru>
2024-11-01 19:09:37 +0400

    В работе модуль 3_1

--- module_3_2.py
+++ module_3_2.py
@@ -1,3 +1,4 @@
+# Функция отправки письма
 def  send_email(message,recipient,sender = "university.help@gmail.com"):
     # Установка флага собаки
     if  not("@" in recipient and "@" in sender):
@@ -15,7 +16,7 @@ def  send_email(message,recipient,sender = "university.help@gmail.com"):
     else:
         flag_sender = False
 
-    # Остаток программы
+    # Проверка, что все поля заполнены (флаги подняты)
     if flag_sobaka and flag_recipient and flag_sender:
         # Проверяме что шлем самому себе
         if recipient == sender:
@@ -24,14 +25,13 @@ def  send_email(message,recipient,sender = "university.help@gmail.com"):
             print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
         elif sender != "university.help@gmail.com":
             print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print("Невозможно отправить письмо с адреса",sender, "на адрес", recipient)
+    print(flag_sobaka, flag_recipient, flag_sender)
 
 
 
-
-
-    print(flag_sobaka,flag_recipient,flag_sender)
 # функция, которая проверит, что кончается на заданную последовательность
-
 def right_ending(what_looking, where_looking):
     flag=False
     for i in range(len(what_looking)):
@@ -45,7 +45,6 @@ def right_ending(what_looking, where_looking):
 
 
 
+# Собственно сама программа
+send_email("rjn yf j,jhjl","vasya@pupkin.ru",sender = "university.help@gmail.com")
 
-
-send_email("rjn yf j,jhjl","vasya@pupkin.ru",sender = "1university.help@gmail.com")
-print(right_ending("qwerty", "123456qwerty"))
