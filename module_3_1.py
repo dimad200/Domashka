9b7bde9a05f7aee9d57de850531872862327a7db
dimad200 <100908404+dimad200@users.noreply.github.com>
2024-11-13 14:24:24 +0400

    Update module_3_1.py
    Исправил

--- module_3_1.py
+++ module_3_1.py
@@ -1,27 +1,25 @@
 def count_calls():
-    global calls
-    calls +=1
-
-
+  global calls
+  calls +=1
+  
 def  string_info(stroka):
-    count_calls()
-    a=len(stroka)
-    b= stroka.upper()
-    c= stroka.lower()
-    result=tuple([len(stroka),stroka.upper(), stroka.lower()])
-    return result
-
+  count_calls()
+  # a=len(stroka)
+  # b= stroka.upper()
+  # c= stroka.lower()
+  result=tuple([len(stroka),stroka.upper(), stroka.lower()])
+  return result
+  
 def is_contains(stroka, spisok):
-    count_calls()
-    a = stroka.lower() # приводим строку к общему виду
-    b=spisok.copy()
-    # приведу список к такому же виду, что и строка
-    for i in range(len(spisok)):
-        b[i]=b[i].lower()
-    # Проверка на вхождение строки в список
-    result = a in b
-    return result
-
+  count_calls()
+  lower_stroka = stroka.lower() # приводим строку к общему виду
+  copy_spisok=spisok.copy()
+  # приведу список к такому же виду, что и строка
+  for i in range(len(spisok)):
+    copy_spisok[i]=copy_spisok[i].lower()
+  # Проверка на вхождение строки в список
+  result = lower_stroka in copy_spisok
+  return result
 
 
 calls =0
@@ -31,4 +29,3 @@ print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
 
 print(calls)
-
