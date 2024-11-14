0ba58c14f0d09b30a5436d200368135700810877
dimad200 <100908404+dimad200@users.noreply.github.com>
2024-11-14 09:33:01 +0400

    Update module_3_2.py
    Исправил

--- module_3_2.py
+++ module_3_2.py
@@ -6,26 +6,26 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
     else:
         flag_sobaka = True
     # Установка флага корректного получателя
-    if recipient.endswith((".com", ".ru", "net")):
+    if recipient.endswith((".com", ".ru", ".net")):
         flag_recipient = True
     else:
         flag_recipient = False
     # Установка флага корректного отправителя
-    if sender.endswith((".com", ".ru", "net")):
+    if sender.endswith((".com", ".ru", ".net")):
         flag_sender = True
     else:
         flag_sender = False
 # Проверка, что адреса почты заданы не верно
     if flag_sobaka == False or flag_recipient == False or flag_sender == False:
         print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
-    # Проверка, что все поля заполнены (флаги подняты) и отправка
-    if flag_sobaka and flag_recipient and flag_sender:
-        # Проверяме что шлем самому себе
-        if recipient == sender:
-            print("Нельзя отправить письмо самому себе!")
-        elif sender == "university.help@gmail.com":
-            print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-        elif sender != "university.help@gmail.com":
+# Проверяме что шлем самому себе
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+ #  Стандартная отправка письма       
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+# Не стандартная отправка письма
+    elif sender != "university.help@gmail.com":
             print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
 
 
@@ -40,5 +40,5 @@ send_email('Напоминаю самому себе о вебинаре', 'urba
 # # еще один тест для проверки, он закомментирован
 # message = 'Вы видите это сообщение как лучший студент курса!'
 # sender = 'urban.info@gmail.com'
-# recipient = 'urban.fan@mail.ru'
+# recipient = 'vasyok1337@gmailТ.net'
 # send_email(message, recipient,sender)
