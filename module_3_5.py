00b0c387c8427b8e5b902a70e27571c25c1cb534
dimad200 <100908404+dimad200@users.noreply.github.com>
2024-11-18 17:57:52 +0400

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -2,19 +2,21 @@
 # Напиши функцию get_multiplied_digits, которая принимает аргумент целое число number и подсчитывает произведение цифр этого числа.
 
 def get_multiplied_digits(number=0):
+  if int(number)==0:
+    return  0
+
   str_number = str(number)
   first=int(str_number[0])
-  if first ==0:
-    return 1
-  if len(str_number)<2:
+
+  if len(str_number)<2 or int(str_number[1:])==0:
+  # если число больше не перебрать, или остались только нули вернем первую цифру
     return first
   else:
-   return first * get_multiplied_digits(int(str_number[1:]))
+    return first * get_multiplied_digits(int(str_number[1:]))
 
 result = get_multiplied_digits(40203)
 
 print(result)
-
 
 # Вывод на консоль:
 # 24
