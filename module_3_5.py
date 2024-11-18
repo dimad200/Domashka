1612739797a9cc57d9023346ab52d153417a36f9
dimad200 <100908404+dimad200@users.noreply.github.com>
2024-11-18 11:31:40 +0400

    Update module_3_5.py
    исправил косяк с последним нулем

--- module_3_5.py
+++ module_3_5.py
@@ -4,6 +4,8 @@
 def get_multiplied_digits(number=0):
   str_number = str(number)
   first=int(str_number[0])
+  if first ==0:
+    return 1
   if len(str_number)<2:
     return first
   else:
@@ -12,6 +14,7 @@ def get_multiplied_digits(number=0):
 result = get_multiplied_digits(40203)
 
 print(result)
+
 
 # Вывод на консоль:
 # 24
